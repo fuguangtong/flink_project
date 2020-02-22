027152f119f10240dc51948eb0d43410dcaced83
FuTong <1986693662@qq.com>
2020-02-22 22:31:28 +0800

    2

---
+++ src/main/java/WordCount.scala
@@ -0,0 +1,16 @@
+import org.apache.flink.api.scala.ExecutionEnvironment
+
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //创建环境
+    val env:ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    //读取文件
+    val input ="F:\\date\\1.txt"
+
+    //有限数据集
+    val dataset=env.readTextFile(input)
+    import org.apache.flink.api.scala._
+    val aggDs = dataset.flatMap(_.split("")).map((_,1)).groupBy(0).sum(1)
+    aggDs.print()
+  }
+}
